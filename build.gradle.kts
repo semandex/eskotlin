9d73474b9e06050a44dd277e138a60b13108218d
Alexander Shvets <33634796+shvets-semandex@users.noreply.github.com>
2023-09-28 17:50:55 -0400

    #4: update client version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,35 +3,38 @@
  */
 
 plugins {
+  id("java-library")
+  id("maven-publish")
   id("org.jetbrains.kotlin.jvm") version "1.9.10"
   id("org.jetbrains.kotlin.plugin.noarg") version "1.9.10"
 
-  id("net.semandex.plugins.gradle-scripts-plugin") version "0.4.6"
+  id("net.semandex.plugins.gradle-scripts-plugin") version "0.4.5"
 }
 
 description = "eskotlin project"
-group "mbuhot"
-version "1.1.0beta1"
+group = "mbuhot"
+version = "1.1.0beta4"
 
 val log4jVersion: String by extra("2.20.0")
 val kotlin_version: String by extra("1.9.10")
 val jacoco_version: String by extra("0.8.8")
 val junit_jupiter_version: String by extra("5.6.2")
-val openSearchVersion = "1.3.13"
+val openSearchVersion = "2.10.0"
 
 pluginScripts {
   fromPlugin("ide")
   fromPlugin("sonarqub")
   fromPlugin("kotlin-jvm17")
-  fromPlugin("kotlin")
   fromPlugin("log4j2")
 
   fromPlugin("junit5")
   fromPlugin("jacoco")
 
   fromPlugin("nexus")
-  fromPlugin("publish")
+  //fromPlugin("publish")
 }
+
+apply(plugin = "maven-publish")
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
   kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
@@ -44,4 +47,13 @@ dependencies {
   implementation("org.opensearch.client:opensearch-rest-high-level-client:$openSearchVersion")
 
   testImplementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+}
+
+publishing {
+  publications {
+    create<MavenPublication>(project.name) {
+      from(components["java"])
+      artifact("sourcesJar")
+    }
+  }
 }
